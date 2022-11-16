c4426374fc6e5b0bb2a2e477162e757ebf0b05b3
karm-patel <karmpatel216@gmail.com>
2022-11-16 11:37:47 +0530

    bug fixed

--- hpca-course-assignment-2022-main/PartA/header/single_thread.h
+++ hpca-course-assignment-2022-main/PartA/header/single_thread.h
@@ -22,10 +22,10 @@ void singleThread(int N, int *matA, int *matB, int *output)
     }
   }
 
-  printf("ARRAY: ");
-    for(int i = 0; i < N; i ++){
-    for(int j = 0; j < N; j ++){
-      printf("%d ", *(output + i*N + j));
+  printf("\nARRAY: ");
+      for(int i = 0; i < N/2; i ++){
+    for(int j = 0; j < N/2; j ++){
+      printf("%d ", output[i*N/2 + j]);
     }
     printf("\n");
   }
@@ -34,18 +34,19 @@ void singleThread(int N, int *matA, int *matB, int *output)
 void singleThread_kij(int N, int *matA, int *matB, int *output)
 {
 
-  for(int i = 0; i < N; i ++){
-    for(int j = 0; j < N; j ++){
-      output[i*N + j] = 0;
+  for(int i = 0; i < N/2; i ++){
+    for(int j = 0; j < N/2; j ++){
+      output[i*N/2 + j] = 0;
     }
   }
 
   assert( N>=4 and N == ( N &~ (N-1)));
   for(int iter = 0; iter < N; iter ++) {
     for(int rowA = 0; rowA < N; rowA += 2){
-      int sum = 0;
+      
       for(int colB = 0; colB < N; colB+=2) 
       {
+        int sum = 0;
         sum += matA[rowA * N + iter] * matB[iter * N + colB];
         sum += matA[(rowA+1) * N + iter] * matB[iter * N + colB];
         sum += matA[rowA * N + iter] * matB[iter * N + (colB+1)];
@@ -62,10 +63,10 @@ void singleThread_kij(int N, int *matA, int *matB, int *output)
     }
   }
 
-  printf("ARRAY kij: ");
-      for(int i = 0; i < N; i ++){
-    for(int j = 0; j < N; j ++){
-      printf("%d ", output[i*N + j]);
+  printf("\nARRAY kij: ");
+      for(int i = 0; i < N/2; i ++){
+    for(int j = 0; j < N/2; j ++){
+      printf("%d ", output[i*N/2 + j]);
     }
     printf("\n");
   }
