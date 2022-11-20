4099c2cd20c2375ee41d2acef4985b2880bba782
karm-patel <karmpatel216@gmail.com>
2022-11-21 01:42:30 +0530

    dynamic __shared__ memory added

--- hpca-course-assignment-2022-main/PartB/header/gpu_thread.h
+++ hpca-course-assignment-2022-main/PartB/header/gpu_thread.h
@@ -19,7 +19,7 @@ void printArray(int N, int *arr){
   printf("-------------------------\n");
 }
 
-__global__ void mat_mul(int N, int *matA, int *matB, int *output){
+__global__ void mat_mul_naive(int N, int *matA, int *matB, int *output){
     int thread_id = (blockIdx.x * blockDim.x) + threadIdx.x;
 
     if (thread_id >= N*N/4)
@@ -51,74 +51,47 @@ __global__ void mat_mul_tiled(int N, int tile, int *matA, int *matB, int *output
     int tx, ty;
     tx = threadIdx.x;
     ty = threadIdx.y;
-    // printf("%d %d\n", tx, ty);
+
     // printf("Blockidx: %d %d Blockdim: %d %d Threads %d, %d\n", blockIdx.x, blockIdx.y, blockDim.x, blockDim.y, tx,ty);
     int rowC = (blockIdx.y * blockDim.y) + ty;
     int colC = (blockIdx.x * blockDim.x) + tx;
     int indexC = rowC * (N>>1) + colC;
 
-    __shared__ int blockA[16][16];
-    __shared__ int blockB[16][16];
+    extern __shared__ int blockAB[]; // contains two array BlockA[tile*tile] & BlockB[tile*tile]
+    int * blockA = blockAB;
+    int * blockB = blockAB + tile*tile;
 
-
-    if (indexC >= 0.5*(N * N)) return;
+    if (indexC >= 0.25*(N * N)) return;
 
     // if (rowC, colC) = (3,2) then this element is made from 
     //C[3][2] = (A[6,:] + A[7,:]) * (B[:,4] + B[:,5])
     int matA_ptr = 2*rowC*N + tx;
     int matB_ptr = 2*colC + ty*N;
 
-    if (indexC == -1)
-        printf("%d: %d %d - - %d %d (%d, %d)\n",indexC, rowC, colC, matA_ptr, matB_ptr, tx, ty);
-    // if (thread_id >= N*N/4)
-    //     return;
-
-    int temp = 0;
+    int sum = 0;
     for (int i =0; i<N; i+=tile){
         
-        // Make small matrix of tile x tile by summing adjecent rows and columns
-        blockA[ty][tx] = matA[matA_ptr] + matA[matA_ptr + N]; // row N + row N+1
-        blockB[ty][tx] = matB[matB_ptr] + matB[matB_ptr + 1]; // col N + col N+1
+        // Make small matrix of size (tile x tile) by summing adjecent rows and columns
+        // We will not use "tx*tile + ty" because Thread(1, 3) located at 3rd row and 1st column in the blockA
+        blockA[ty*tile + tx] = matA[matA_ptr] + matA[matA_ptr + N]; // row N + row N+1
+        blockB[ty*tile + tx] = matB[matB_ptr] + matB[matB_ptr + 1]; // col N + col N+1
 
         // wait for all threads to fill blockA and blockB completely
         __syncthreads();
 
-        if (indexC == -1)
-        {
-        printf("\nA = \n");
-        for (int a =0; a<tile; a++){
-            for (int b =0; b<tile; b++){
-                printf("blockA[%d][%d] = %d ",a, b, blockA[a][b]);
-            }
-            printf("\n");  
-        }
-        printf("\nB: \n");
-        for (int a =0; a<tile; a++){
-            for (int b =0; b<tile; b++){
-                printf("blockB[%d][%d] = %d ",a, b, blockB[a][b]);
-            }
-            printf("\n");  
-        }
-        }
-
         // fill blockC = blockA * blockB 
-        // current thread will multiply tx th row of A with ty column of B 
-        // temp = blockC[tx][ty]
+        // current thread will multiply tx row of A with ty column of B ~ naive kernel
+        // where, sum = blockC[tx][ty]
         for (int a=0; a<tile; a++){
-            temp += blockA[ty][a]*blockB[a][tx];
-            // if (indexC == 0)
-            //     printf("%d * %d + ",blockA[tx][a],blockB[a][ty]);
-
+            sum += blockA[ty*tile + a]*blockB[a*tile + tx];
         }
         
-        matA_ptr += tile;
-        matB_ptr += N*tile;
+        matA_ptr += tile; // row vise increment
+        matB_ptr += N*tile; // column vise increment
 
         __syncthreads();
     }
-
-    output[indexC] = temp;
-
+    output[indexC] = sum;
 }
 
 // Fill in this function
@@ -126,7 +99,7 @@ void gpuThread(int N, int *matA, int *matB, int *output)
 {
 
     ini_zero(N/2, output);
-    cudaSetDevice(2);
+    cudaSetDevice(4);
     size_t bytes = sizeof(int) * N * N;
 
     // Assign memory from GPU to gpu arrays
@@ -142,7 +115,7 @@ void gpuThread(int N, int *matA, int *matB, int *output)
 
 
     int kernel = 1;
-    int TILE = 16;
+    int TILE = 128;
     dim3 threadsPerBlock(TILE, TILE);
     dim3 numBlocks(0.5*N/TILE, 0.5*N/TILE);
 
@@ -152,11 +125,11 @@ void gpuThread(int N, int *matA, int *matB, int *output)
             int N_THREADS_PER_BLOCK, N_BLOCKS;
             N_THREADS_PER_BLOCK = 1 << 10;
             N_BLOCKS = (N*N/4 + N_THREADS_PER_BLOCK - 1) / N_THREADS_PER_BLOCK;
-            mat_mul <<< N_BLOCKS, N_THREADS_PER_BLOCK >>> (N, matA_gpu, matB_gpu, output_gpu);
+            mat_mul_naive <<< N_BLOCKS, N_THREADS_PER_BLOCK >>> (N, matA_gpu, matB_gpu, output_gpu);
         break;
 
         case 1: // Tiled implementation
-            mat_mul_tiled <<<numBlocks, threadsPerBlock >>> (N, TILE, matA_gpu, matB_gpu, output_gpu);
+            mat_mul_tiled <<<numBlocks, threadsPerBlock, 2*TILE*TILE*sizeof(int)>>> (N, TILE, matA_gpu, matB_gpu, output_gpu);
             break;
 
         default:
@@ -165,15 +138,13 @@ void gpuThread(int N, int *matA, int *matB, int *output)
 
     }  
 
+    // This will acts as barries for all threads
     cudaMemcpy(output, output_gpu, bytes/4, cudaMemcpyDeviceToHost);
     
-    // printf("freed");
     //Free memory on gpu
     cudaFree(matA_gpu);
     cudaFree(matB_gpu);
     cudaFree(output_gpu);
 
-    // printArray(N/2, output);
     std::cout << "Multiplied\n";
-    
     }
