303528ea70699f4e27e4485a436cd1db7d3931b9
karm-patel <karmpatel216@gmail.com>
2022-11-25 15:18:01 +0530

    some minor changes

--- hpca-course-assignment-2022-main/PartB/slurm.sh
+++ hpca-course-assignment-2022-main/PartB/slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --job-name=mat_mul
-#SBATCH --output=results/mat_mul_16384_tiled_128
+#SBATCH --output=results/mat_mul_4096_tiled_128
 nvcc main.cu -o cmm_server -I ./header
-./cmm_server data/input_16384.in
+./cmm_server data/input_4096.in
 nvidia-smi
